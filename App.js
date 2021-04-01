0d09fcc9eb478935e0306fdf47155e477f98b0c9
Alfonso <alfonsodelag1@gmail.com>
2021-04-01 17:07:27 +0200

    adding files

--- App.js
+++ App.js
@@ -1,21 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StyleSheet } from 'react-native';
+import 'react-native-gesture-handler';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import NuevaOrden from './views/NuevaOrden';
+import Menu from './views/Menu';
+import FormularioPlatillo from './views/FormularioPlatillo';
+import ResumenPedido from './views/ResumenPedido';
+import ProgresoPedido from './views/ProgresoPedido';
+import DetallePlatillo from './views/DetallePlatillo';
+
+// Importar state de Context
+import FirebaseState from "./context/firebase/firebaseState";
+import PedidoState from "./context/pedidos/pedidosState";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <FirebaseState>
+        <PedidoState>
+          <NavigationContainer>
+            <Stack.Navigator
+              screenOptions={{
+                headerStyle: {
+                  backgroundColor: "#FFDA00"
+                }, TextheadreTitleStyle: {
+                  fontWeight: "bold"
+                }
+              }}
+            >
+              <Stack.Screen
+                name="NuevaOrden"
+                component={NuevaOrden}
+                options={{
+                  title: "Nueva Orden"
+                }}
+              />
+              <Stack.Screen
+                name="Menu"
+                component={Menu}
+                options={{
+                  title: "Menu"
+                }}
+              />
+              <Stack.Screen
+                name="DetallePlatillo"
+                component={DetallePlatillo}
+                options={{
+                  title: "Detalle Platillo"
+                }}
+              />
+
+              <Stack.Screen
+                name="FormularioPlatillo"
+                component={FormularioPlatillo}
+                options={{
+                  title: "Ordenar Platillo"
+                }}
+              />
+              <Stack.Screen
+                name="ResumenPedido"
+                component={ResumenPedido}
+                options={{
+                  title: "ResumenPedido"
+                }}
+              />
+              <Stack.Screen
+                name="ProgresoPedido"
+                component={ProgresoPedido}
+                options={{
+                  title: "Progreso de Pedido"
+                }}
+              />
+
+            </Stack.Navigator>
+          </NavigationContainer>
+        </PedidoState>
+      </FirebaseState>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
